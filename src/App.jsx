f2d5cc74931d5bf1f3f2fe267e6d37ea7caba0dc
object-ions <moshikoatia@gmail.com>
2023-11-28 21:21:15 -0800

    Init files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+  
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li><span className="player-name">Player 1</span></li>
+          <span className="player-symbol">X</span>
+          
+          <li><span className="player-name">Player 2</span></li>
+          <span className="player-symbol">0</span>
+        </ol>
+
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
+  )
+}
+
+export default App
